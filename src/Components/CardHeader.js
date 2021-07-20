60d79550b25814066ba15e43fff665a2d525b5ad
Veranika Viarbitskaya <vviarbitskaya@gomel.iba.by>
2021-07-20 10:14:49 +0300

    Task 2 Updated

--- src/Components/CardHeader.js
+++ src/Components/CardHeader.js
@@ -3,14 +3,17 @@ import './CardHeader.css';
 import MyCheckBox from './CheckBox';
 
 const CardHeader = props => {
-  
-  const [isActive, setActive] = useState("false");
+  const [isActive, setActive] = useState('false');
 
   const handleToggle = () => {
     setActive(!isActive);
   };
 
-  return <div className={isActive ? "card-header" : "card-header-checked"}>{props.cardHeader} <MyCheckBox handleToggle = {handleToggle}/> </div> 
+  return (
+    <div className={isActive ? 'card-header' : 'card-header-checked'}>
+      {props.cardHeader} <MyCheckBox handleToggle={handleToggle} />{' '}
+    </div>
+  );
 };
 
 export default CardHeader;
--- src/Components/CheckBox.js
+++ src/Components/CheckBox.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import './CheckBox.css';
 
-const MyCheckBox = ({handleToggle}) => {
-    return <input type ="checkbox" color="default" onChange ={handleToggle}></input>
+const MyCheckBox = ({ handleToggle }) => {
+  return (
+    <input
+      className="check-box"
+      type="checkbox"
+      color="default"
+      onChange={handleToggle}
+    ></input>
+  );
 };
 export default MyCheckBox;
